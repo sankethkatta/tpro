78267b639633cdc31f4dc7404663c40fa61a5aa9
Anthony Salgado <asalgadox@gmail.com>
2012-11-29 22:32:50 -0800

    First pass at the database schema

--- src/models.py
+++ src/models.py
@@ -6,32 +6,52 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/tpro.db'
 db = SQLAlchemy(app)
 
 class BaseModel(db.Model):
-    
+   
+    # every model inherits from BaseModel, so they all have an ID
     id = db.Column(db.Integer, primary_key=True)
+    __abstract__ = True
 
     def save(self):
         """ One-hit commit function """
         db.session.add(self)
         db.session.commit()
 
+class Industry(BaseModel):
+
+    __tablename__ = 'industries'
+    name = db.Column(db.String(80), unique=True)
+
+class Stem(BaseModel):
+
+    __tablename__ = 'stems'
+    text = db.Column(db.String(80), unique=True)
+
 class Term(BaseModel):
 
     __tablename__ = 'terms'
-    term = db.Column(db.String(80), unique=True)
+    tweet_id = db.Column(db.Integer, db.ForeignKey('tweets.id'))
+    stem_id = db.Column(db.Integer, db.ForeignKey('stems.id'))
+    text = db.Column(db.String(80), unique=True)
 
-    def __init__(self, term):
-        self.term = term
+    def __init__(self, text):
+        self.text = text
 
     def __repr__(self):
-        return 'Term(%r)' % self.term
-        
+        return 'Term(%r)' % self.text
+       
+class Tweet(BaseModel):
+
+    __tablename__ = 'tweets'
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    text = db.Column(db.String(200), nullable=False)
+    
 
 class User(BaseModel):
 
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
+    industry_id = db.Column(db.Integer, db.ForeignKey('industries.id'))
 
     def __init__(self, username, email):
         self.username = username
