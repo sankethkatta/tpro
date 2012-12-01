12063ee62fc9a61225841d9498cf358b6b210c8e
Anthony Salgado <asalgadox@gmail.com>
2012-12-01 14:17:15 -0800

    adding the updated version of models

--- src/models.py
+++ src/models.py
@@ -1,9 +1,23 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from views import app
+from stemming.porter import stem as stemmer
+import csv
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/tpro.db'
 db = SQLAlchemy(app)
+
+
+INDUSTRIES = dict(
+    politics=["whitehouse", "KarlRove", "SenJohnMcCain", "nprpolitics", 
+              "SpeakerBoehner", "chavezcadanga", "thehill", "johnboehner", 
+              "GStephanopoulos", "JimDeMint", 'BarackObama'],
+
+    sports=["espn", "KingJames", "mcuban", "DwyaneWade", "WojYahooNBA",
+            "SInow", "drewbrees", "MichelleDBeadle", "darrenrovell", 
+            "JayGlazer"],
+
+)
 
 class BaseModel(db.Model):
    
@@ -15,29 +29,49 @@ class BaseModel(db.Model):
         """ One-hit commit function """
         db.session.add(self)
         db.session.commit()
+        return self
 
 class Industry(BaseModel):
 
     __tablename__ = 'industries'
     name = db.Column(db.String(80), unique=True)
 
+    @staticmethod
+    def get_or_create(name):
+        industry = Industry.query.filter_by(name=name).first()
+        if not industry:
+            industry = Industry(name=name).save()
+        return industry
+
 class Stem(BaseModel):
 
     __tablename__ = 'stems'
     text = db.Column(db.String(80), unique=True)
+
+    @staticmethod
+    def get_or_create(text):
+        stem = Stem.query.filter_by(text=text).first()
+        if not stem:
+            stem = Stem(text=text).save()
+        return stem
 
 class Term(BaseModel):
 
     __tablename__ = 'terms'
     tweet_id = db.Column(db.Integer, db.ForeignKey('tweets.id'))
     stem_id = db.Column(db.Integer, db.ForeignKey('stems.id'))
-    text = db.Column(db.String(80), unique=True)
+    text = db.Column(db.String(80))
 
-    def __init__(self, text):
-        self.text = text
 
     def __repr__(self):
         return 'Term(%r)' % self.text
+
+    @staticmethod
+    def get_or_create(text, tweet_id, stem_id):
+        term = Term.query.filter_by(text=text).first()
+        if term is None:
+            term = Term(tweet_id=tweet_id, stem_id=stem_id, text=text).save()
+        return term
        
 class Tweet(BaseModel):
 
@@ -50,12 +84,45 @@ class User(BaseModel):
 
     __tablename__ = 'users'
     username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
+    email = db.Column(db.String(120))
     industry_id = db.Column(db.Integer, db.ForeignKey('industries.id'))
-
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
 
     def __repr__(self):
         return 'User(%r)' % self.username
+
+    @staticmethod
+    def get_or_create(username, industry_id=None):
+        user = User.query.filter_by(username=username).first()
+        if not user:
+            user = User(username=username, industry_id=industry_id).save()
+        return user
+
+from nltk.tokenize import word_tokenize
+
+def import_csv_file(industry_name, fname):
+    # find the industry in our DB
+    industry = Industry.get_or_create(industry_name)
+    assert industry is not None, "something bad happened when creating an industry"
+    with open(fname, 'r') as f:
+        for line in f:
+            reader = csv.reader(f)
+            for row in reader:
+                try:
+                    username, tweet_text = row
+                    user = User.get_or_create(username, industry_id=industry.id)
+                    tweet = Tweet(user_id=user.id, text=tweet_text).save()
+                    for token in word_tokenize(tweet_text.lower()):
+                        stem = Stem.get_or_create(stemmer(token)) 
+                        term = Term.get_or_create(token, tweet.id, stem.id)
+                except Exception as err:
+                    print err
+
+if __name__ == '__main__':
+    for industry, users in INDUSTRIES.iteritems():
+        for username in users:
+            try:
+                import_csv_file(industry, '%s.csv' % username)
+            except Exception as err:
+                print err
+
+
