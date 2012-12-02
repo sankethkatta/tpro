80baa1dac418e52f1f2e14efd0d8589dfa42ccce
Sanketh Katta <me@sankethkatta.com>
2012-12-02 13:27:59 -0800

    finished data retrival

--- scraper/tweetscraper.py
+++ scraper/tweetscraper.py
@@ -69,8 +69,8 @@ def ROLL_KEY():
 ROLL_KEY()
 
 users = [username.lower() for username in users]
-#failed indices: 381, 504, 583, 625, 897
-for i in xrange(912, len(users)):
+#failed indices: 381, 504, 583, 625, 897, 912
+for i in xrange(1005, len(users)):
     print "User: %s, Index: %d" % (users[i], i)
     data = []
     max_id=None
