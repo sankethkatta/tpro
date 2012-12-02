6c21ea141217cb2707b6fe1dba82359bbf9560c4
Sanketh Katta <me@sankethkatta.com>
2012-12-01 23:17:39 -0800

    new data

--- src/twitter_utils/user.py
+++ src/twitter_utils/user.py
@@ -1,10 +1,13 @@
 from twython import Twython
+from topusers import users
+import json
 
-t = Twython(app_key='RzvEfd2YnuLG4HLTamB71A',
-            app_secret='23ddxWtwmXXWTfBPz3S0MJRkKXxXailx2fI40ehd2M0',
-            oauth_token='15202847-wcK11pamFZO4w6pG4LHfnOtVf5YnvKHeG1voK0Y10',
-            oauth_token_secret='ou7dnysvPNFjPMN5QBKnvXSjn1m490WQpOAFIdDlfQ')
+t = Twython(app_key='fxmq7LkdyUpQbTh72zqTA',
+            app_secret='kSNCIvAOZxy4zipLzNWJxjPQ9KvOROaX1HTwHjufdc',
+            oauth_token='15202847-lOjmNyFbxjImFOESPjGDue7mVgYuy9868Aghzurw',
+            oauth_token_secret='XK0wDtdYfTW77bKv8RuQD1lqXuVpilaYCoEZ3YohUA')
 
+file = open("topuserimages.py", "w")
 def get_profileimage_urls(users):
     """
     Input: list of twitter handles
@@ -12,6 +15,12 @@ def get_profileimage_urls(users):
     """
     urls = {}
     for user in users:
-        urls[user] = (t.getProfileImageUrl(user, size="bigger"))
+        try:
+            urls[user] = (t.getProfileImageUrl(user, size="bigger"))
+        except:
+            pass
+        print user
+    json.dump(urls, file)
 
-    return urls
+if __name__ == "__main__":
+    get_profileimage_urls(users)
