4812a4b1d32a12309d81a76dde2f2456a27d3330
Sanketh Katta <me@sankethkatta.com>
2012-12-02 00:13:24 -0800

    added api key list, and more data

--- scraper/tweetscraper.py
+++ scraper/tweetscraper.py
@@ -7,13 +7,28 @@ import os
 # This script gets all public tweets(including retweets) of a user.
 # To run this: python tweetscraper.py <username>
 # You will find a csv with a tokenized list of tweets in username.csv
-api = twitter.Api(consumer_key = '7i7MFfbiHCP4E7i1l9S2g',
-                   consumer_secret='jGwDVgkdFu7GZIntPnyBq7wSgtTurtlYwvXAfRbxCPY',
-                   access_token_key = '15202847-lOjmNyFbxjImFOESPjGDue7mVgYuy9868Aghzurw',
-                   access_token_secret = 'XK0wDtdYfTW77bKv8RuQD1lqXuVpilaYCoEZ3YohUA')
+
+API_KEYS = [
+    dict(consumer_key = 'N0D7AL9QKSivv5Ogrbunxg',
+         consumer_secret='JCf5pL8n9SiWf82cDp1rcS165HFS648vEXf5W5g0',
+         access_token_key = '984137052-8qrvCtFTi7GAXRVRyhWhAthBzBuldBK9ETYAOHE',
+         access_token_secret = 'u7bPrucjteHBRsKhOw8wpOWmWMKCTAk34YDaKtcX6sA'),
+
+    dict(consumer_key = '7i7MFfbiHCP4E7i1l9S2g',
+         consumer_secret='jGwDVgkdFu7GZIntPnyBq7wSgtTurtlYwvXAfRbxCPY',
+         access_token_key = '15202847-lOjmNyFbxjImFOESPjGDue7mVgYuy9868Aghzurw',
+         access_token_secret = 'XK0wDtdYfTW77bKv8RuQD1lqXuVpilaYCoEZ3YohUA'),
+]
+
+CUR_KEY = 0 
+
+api = twitter.Api(consumer_key = API_KEYS[CUR_KEY]['consumer_key'],
+                   consumer_secret = API_KEYS[CUR_KEY]['consumer_secret'],
+                   access_token_key = API_KEYS[CUR_KEY]['access_token_key'],
+                   access_token_secret = API_KEYS[CUR_KEY]['access_token_secret'])
 
 users = [username.lower() for username in users]
-for i in xrange(80, 500):
+for i in xrange(149, 500):
     print "User: %s, Index: %d" % (users[i], i)
     data = []
     max_id=None
