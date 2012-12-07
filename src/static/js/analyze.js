7719171ae142c0a4d3971191f04311d28e6af496
sankethkatta <sankethkatta@gmail.com>
2012-12-07 06:33:36 +0000

    @handle on frontend

--- src/static/js/analyze.js
+++ src/static/js/analyze.js
@@ -3,7 +3,7 @@ var loading = $("#loading");
 var button = $("#tweetinput button");
 var result_list = $("#result_list");
 var template = function(user, img, bio) {
-	return '<li > <div class="span3"> <a href="http://twitter.com/'+user+'" > <img class="img-circle" src="'+img+'"  data-content="'+bio+'" data-original-title="Biography"> <iframe allowtransparency="true" frameborder="0" scrolling="no" src="//platform.twitter.com/widgets/follow_button.html?screen_name='+user+'&show_count=false" style="width:100%; height:25px;"></iframe> </div><!-- /.span3 --> </li>'
+	return '<li > <div class="span3"> <a href="http://twitter.com/'+user+'" target="_blank" > <img class="img-circle" src="'+img+'"  data-content="'+bio+'" data-original-title="Biography"> <iframe allowtransparency="true" frameborder="0" scrolling="no" src="//platform.twitter.com/widgets/follow_button.html?screen_name='+user+'&show_count=false" style="width:100%; height:25px;"></iframe> </div><!-- /.span3 --> </li>'
 
 };
 $("#tweetinput").submit(function(e) {
@@ -21,6 +21,7 @@ $("#tweetinput").submit(function(e) {
         data: query
     }).done(function(data) {
        if (data.length === 0) {
+	   loading.css("display", "none");
 	  flashMessage();
 	} else {
 	   loading.css("display", "none");
